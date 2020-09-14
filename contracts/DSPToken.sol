c17d1303bea44c34a606b7c49b55338063c12d12
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-09-14 16:42:31 +0530

    added name, symbol and standard of token

--- contracts/DSPToken.sol
+++ contracts/DSPToken.sol
@@ -6,8 +6,14 @@ contract DSPToken {
     //read the total number of tokens
 
     uint256 public totalSupply;
+    string public name = 'DSP Token';
+    string public symbol = 'DSP';
+    string public standard = 'DSP Token v1.0';
+
+    mapping(address => uint256) public balanceOf;
 
     constructor(uint256 _initialSupply) public {
+        balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
     }
 }
