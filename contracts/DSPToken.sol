5882b5c2af5b741c300ba8ea646771dcb41936bb
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2020-09-16 12:22:29 +0530

    transfer tokens function

--- contracts/DSPToken.sol
+++ contracts/DSPToken.sol
@@ -10,10 +10,24 @@ contract DSPToken {
     string public symbol = 'DSP';
     string public standard = 'DSP Token v1.0';
 
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint _value
+    );
+
     mapping(address => uint256) public balanceOf;
 
     constructor(uint256 _initialSupply) public {
         balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
+    }
+
+    function transfer(address _to, uint _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
     }
 }
